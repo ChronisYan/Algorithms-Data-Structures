5c352741c3455de17adb4f0d4c7a53f32cb97141
ChronisG <thegamedevelopergame@gmail.com>
2020-04-21 17:09:19 +0100

    improved sorting code/ added linear search

--- javascript/sorting-algorithms.js
+++ javascript/sorting-algorithms.js
@@ -5,13 +5,15 @@ class RandomArray {
   constructor() {
     this.array = [];
   }
-  generate() {
+
+  get generate() {
     for (let i = 0; i < 1000; i++) {
       const num = Math.floor(Math.random() * 501);
       this.array.push(num);
     }
   }
-  suffle() {
+
+  get suffle() {
     const { array } = this;
     let newPos, temp;
     for (let i = array.length - 1; i > 0; i--) {
@@ -21,122 +23,109 @@ class RandomArray {
       array[newPos] = temp;
     }
   }
+
   get display() {
     console.log(this.array);
   }
-}
-const array1 = new RandomArray();
-array1.generate();
 
-//--------------------------------------------------------
+  //#######################################################
+  // BUBBLE SORT
+  bubbleSort() {
+    const { array } = this;
+    let temp,
+      swap = false;
 
-// BUBBLE SORT
-RandomArray.prototype.bubbleSort = function () {
-  const { array } = this;
-  // IMPLEMENTATION
-  let temp,
-    swap = false;
-  do {
-    swap = false;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] > array[i + 1]) {
-        temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
-        swap = true;
+    do {
+      swap = false;
+      for (let i = 0; i < array.length; i++) {
+        if (array[i] > array[i + 1]) {
+          temp = array[i];
+          array[i] = array[i + 1];
+          array[i + 1] = temp;
+          swap = true;
+        }
       }
-    }
-  } while (swap);
-
-  this.display; // display sorted array
-};
-// array1.bubbleSort()
-
-array1.suffle(); //re-suffle array
-
-//--------------------------------------------------------
-
-// INSERTION SORT
-RandomArray.prototype.insertionSort = function () {
-  const { array } = this;
-
-  //IMPLEMENTATION
-  for (let i = 1; i < array.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (array[i] < array[j]) {
-        const temp = array.splice(i, 1);
-        array.splice(j, 0, temp[0]); //temp is an array with lenght = 1
-        break;
-      }
-    }
+    } while (swap);
+    this.display;
   }
 
-  this.display; // display sorted array
-};
-// array1.insertionSort()
+  //#######################################################
+  //INSERTION SORT
+  insertionSort() {
+    const { array } = this;
 
-array1.suffle(); //re-suffle array
-
-//--------------------------------------------------------
-
-// MERGE SORT
-RandomArray.prototype.mergeSort = function () {
-  const { array } = this;
-
-  // //IMPLEMENTATION
-  const mergeSort = (arr) => {
-    if (arr.length < 2) return arr;
-
-    const mid = Math.floor(arr.length / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
-
-    return merge(mergeSort(left), mergeSort(right));
-  };
-  const merge = (left, right) => {
-    const result = [];
-
-    while (left.length && right.length) {
-      if (left[0] < right[0]) {
-        result.push(left.shift());
-      } else {
-        result.push(right.shift());
+    for (let i = 1; i < array.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (array[i] < array[j]) {
+          const temp = array.splice(i, 1);
+          array.splice(j, 0, temp[0]); //temp is an array with lenght = 1
+          break;
+        }
       }
     }
+    this.display;
+  }
+  //#######################################################
+  // MERGE SORT
+  mergeSort() {
+    const { array } = this;
 
-    return result.concat(left, right);
-  };
+    const mergeSort = (arr) => {
+      if (arr.length < 2) return arr;
 
-  this.array = mergeSort(array);
-  this.display; // display sorted array
-};
-// array1.mergeSort()
+      const mid = Math.floor(arr.length / 2);
+      const left = arr.slice(0, mid);
+      const right = arr.slice(mid);
 
-array1.suffle(); //re-suffle array
+      return merge(mergeSort(left), mergeSort(right));
+    };
+    const merge = (left, right) => {
+      const result = [];
 
-//--------------------------------------------------------
+      while (left.length && right.length) {
+        if (left[0] < right[0]) {
+          result.push(left.shift());
+        } else {
+          result.push(right.shift());
+        }
+      }
 
-// QUICK SORT
+      return result.concat(left, right);
+    };
+    this.array = mergeSort(array);
+    this.display;
+  }
+  //#######################################################
+  // QUICK SORT
+  quickSort() {
+    const { array } = this;
 
-RandomArray.prototype.quickSort = function () {
-  const { array } = this;
+    const quickSort = (arr) => {
+      if (arr.length < 2) return arr;
 
-  const quickSort = (arr) => {
-    if (arr.length < 2) return arr;
+      const pivot = arr[arr.length - 1],
+        left = [],
+        right = [];
 
-    const pivot = arr[arr.length - 1],
-      left = [],
-      right = [];
+      for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+      }
 
-    for (let i = 0; i < arr.length - 1; i++) {
-      arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
-    }
+      return quickSort(left).concat(pivot, quickSort(right));
+    };
 
-    return quickSort(left).concat(pivot, quickSort(right));
-  };
+    this.array = quickSort(array);
+    this.display;
+  }
+}
 
-  this.array = quickSort(array);
-  this.display; // display sorted array
-};
+//-------------------------------------------
 
-array1.quickSort();
+const array1 = new RandomArray();
+array1.generate;
+
+console.log(array1.quickSort());
+
+// array1.suffle;
+// console.log(array1.display);
+// console.log(array1.mergeSort());
