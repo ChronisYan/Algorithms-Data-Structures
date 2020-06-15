64576cf919c4a972b50a5475496105de54b5bf37
ChronisG <thegamedevelopergame@gmail.com>
2020-06-15 22:35:19 +0100

    fix

--- C++/intro-math/number-theory.cpp
+++ C++/intro-math/number-theory.cpp
@@ -5,5 +5,5 @@ int main()
     // Calculate sum of N first natural number:
     // n*(n+1) / 2
     int n = 8;
-    printf("Sum of %d first numbers: %d", n, n * (n + 1) / 2);
+    printf("Sum of %d first numbers: %d\n", n, n * (n + 1) / 2);
 }
