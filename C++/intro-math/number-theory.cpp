20dfe35c05023fb2cb57e373ab0cc81207c6cbe4
ChronisG <thegamedevelopergame@gmail.com>
2020-06-15 22:33:46 +0100

    first c++ update gitignore

---
+++ C++/intro-math/number-theory.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    // Calculate sum of N first natural number:
+    // n*(n+1) / 2
+    int n = 8;
+    printf("Sum of %d first numbers: %d", n, n * (n + 1) / 2);
+}
