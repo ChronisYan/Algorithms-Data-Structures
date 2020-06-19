0846d757afa2a0c8617e3fcf7b489d58846289f1
ChronisG <thegamedevelopergame@gmail.com>
2020-06-19 18:16:08 +0100

    c++ DS set up

---
+++ C++/data-structures/binaryTree.cpp
@@ -0,0 +1,12 @@
+// A binary tree is a hierarchical data structure.
+
+// The topmost node is called the root.
+// Each node can have up to two children, hence the name Binary tree.
+// The two children of a node are the left and right children.
+// The node above a node is called its parent. Root has no parent.
+// Nodes with no children are called leaves or external nodes.
+// Non-Leaf nodes are called internal nodes.
+// The number of levels is the height of the tree.
+// Siblings: Nodes with the same parent.
+// Descendant: Node reachable by traversing children (Nodes in the Subtree).
+// Ancestor: Nodes reachable by traversing parents (Nodes on the path from the node to root).
---
+++ C++/data-structures/stack.cpp
@@ -0,0 +1,10 @@
+// C++ STL
+
+// stack<int> S; //empty stack
+
+// int x = S.top(); // get top element
+// S.pop(); // pop top element
+// S.push(x); //push x on top
+
+// bool x = S.empty(); // to check if stack is empty
+// int sz = S.size(); // get size of stack
