324cdf043ad135e09325cfc7985ff1abc8e5d8c2
ChronisG <thegamedevelopergame@gmail.com>
2020-07-20 16:19:40 +0100

    add C dir

---
+++ C/basics/arrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare an array of size 5
+    int A[5];
+
+    // Declare and Initiate an array of size n
+    int B[4] = {3, 6, 8, 10};
+
+    // Traverse an array
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d ", B[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
