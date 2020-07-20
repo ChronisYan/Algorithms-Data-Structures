266855e181fed4668ad43a4f8cfae360a6a1ff46
ChronisG <thegamedevelopergame@gmail.com>
2020-07-20 17:42:43 +0100

    add C basics

--- C/basics/structures.c
+++ C/basics/structures.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+// structs can be used to define our own custon data types
+
+// struct definition (don't occupy memory)
+struct Rectangle
+{
+    int length;
+    int width;
+};
+
 int main()
 {
+
+    // struct declaration (take as much memory as the sum of the memory occupied by each of its members)
+    struct Rectangle rect1;
+
+    // struct declaration and initialization
+    struct Rectangle rect2 = {10, 5};
+
+    // accessing members of a struct
+    rect1.length = 90;
+    rect1.width = 45;
+    rect2.width = 15;
+
+    // Array of structs
+    struct Rectangle rect_list[3] = {{2, 4}, {50, 4}, rect2};
+
+    printf("Area of rect2: %d\n", rect_list[2].length * rect_list[2].width);
+
     return 0;
 }
