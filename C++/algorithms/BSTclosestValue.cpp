e460c1ff4b4a8f8fbd543a52af6158a79704da95
ChronisG <thegamedevelopergame@gmail.com>
2020-07-25 18:16:26 +0100

    add c ds and c++ bst algo

--- C++/algorithms/BSTclosestValue.cpp
+++ C++/algorithms/BSTclosestValue.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
-
 // Find the colest value in a BST
 // A valid BST node:
 // its value is greater than the values of every node to its left
 // its value is less than or equal to the values of every node to its right
 // its children nodes are either valid BST nodes themselves or null
+#define NULL nullptr
 
 class BST
 {
@@ -17,12 +16,39 @@ public:
     BST &insert(int val);
 };
 
-int findClosestValueInBst(BST *tree, int target)
+int abs(int a, int b)
 {
-    // Write your code here.
-    return -1;
+    if (a > b)
+        return a - b;
+    else
+        return b - a;
 }
 
-int main()
+int findClosestValueInBst(BST *tree, int target)
 {
+    BST *curNode = tree;
+    int result = tree->value;
+    int score_to_beat = abs(tree->value, target);
+    while (curNode != NULL)
+    {
+        int curDifference = abs(curNode->value, target);
+        if (curDifference < score_to_beat)
+        {
+            result = curNode->value;
+            score_to_beat = curDifference;
+        }
+        if (curNode->value > target)
+        {
+            curNode = curNode->left;
+        }
+        else if (curNode->value < target)
+        {
+            curNode = curNode->right;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
 }
