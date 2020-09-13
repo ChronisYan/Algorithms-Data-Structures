f6d1817c65eb6a20e9ed966d1367d6cbeba3bdf9
ChronisG <thegamedevelopergame@gmail.com>
2020-09-13 04:37:03 +0100

    finish basic linked list in c++

--- C++/data-structures/linkedList.cpp
+++ C++/data-structures/linkedList.cpp
@@ -5,22 +5,36 @@ template <typename T>
 class List
 {
 public:
+    List() : head_(nullptr){};
     const T &operator[](unsigned index)
     {
         Node *cur_node = head_;
 
         while (index > 0)
         {
-            if (cur_node->next == nullptr)
-                throw std::out_of_range("Index is out of range");
+            if (cur_node == nullptr || cur_node->next == nullptr)
+                throw std::out_of_range("index is out of range");
 
             cur_node = cur_node->next;
             index--;
         }
         return cur_node->data;
     }
+    // std::cout displaying function
+    friend std::ostream &operator<<(std::ostream &os, List &list)
+    {
+        Node *cur_node = list.head_;
+
+        while (cur_node != nullptr)
+        {
+            std::cout << cur_node->data << " -> ";
+            cur_node = cur_node->next;
+        }
+        return os;
+    }
+
     // adds element at the front of the list
-    void insert(const T &data)
+    void insert(T data)
     {
         // create new node
         Node *new_node = new Node(data);
@@ -34,18 +48,25 @@ private:
     class Node
     {
     public:
-        const T &data;
+        T data;
         Node *next;
-        Node(const T &data) : data(data), next(nullptr){};
+        Node(T data) : data(data), next(nullptr){};
     };
-
+    // list.insert(4);
+    // list.insert(1);
+    // list.insert(5);
     Node *head_; // pointer to the first element of the list
 };
 
 int main()
 {
+    List<int> list;
 
-    std::cout << "Hello" << std::endl;
+    list.insert(4);
+    list.insert(1);
+    list.insert(5);
+
+    std::cout << list << std::endl;
 
     return 0;
 }
